3d6dd472201fd98affe763437994084ca4b581db
inker610566 <inker610566@yahoo.com.tw>
2015-01-06 12:30:05 +0800

    under dev

--- NepServer/NepServer/nepserver.py
+++ NepServer/NepServer/nepserver.py
@@ -1,23 +1,39 @@
+import ..third_party.bottle.bottle
+from ..third_party.bottle.bottle import Bottle
+from ..third_party.bottle.bottle import request
 '''
 	Provide common server interface to vim client.
 GDBServer/LLDBServer will extend this interface for communicate with vim client.
 An instance of NepServer controls only one debugger process(one NepDbg).
 '''
 
+bottle.Request.MEMFILE_MAX = 1000 * 1024
+
 class NepServer:
-	def __init__(self, source_file):
+	def __init__(self):
 		# self.debugger = new NepDbg()
 		self._InstallServer()
 	
-	def CreateBreakpoint(self, ):
+	def CreateBreakpoint(self, source_file, line):
 		pass
 	
 	def _InstallServer(self):
-		self._InstallRESTful()
+		app = Bottle()
+		self._InstallRouting(app)
+		app.run(host='localhost', port=5566)
 
-	def _InstallRESTful(self):
-		pass
+	def _InstallRouting(self, app):
+		app.route('/breakpoint', 'GET', lambda *args, **kargs: self._getBreakpoint(*args, **kargs))
+		app.route('/breakpoint', 'POST', lambda *args, **kargs: self._postBreakpoint(*args, **kargs))
 
-	def _InstallHandler(self):
-		import 
+	'''
+		RESTful handler
+	'''
+	def _getBreakpoint(self):
+		return "Hello"
 	
+	def _postBreakpoint(self):
+		# request.json
+		return "Hello"
+
+
